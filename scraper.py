b4469f66c36dcaf2e3ccbd72663ab7bf7d80c009
José Paulo <petrybr@gmail.com>
2021-06-27 19:27:46 -0300

    fixed sleep

--- scraper.py
+++ scraper.py
@@ -1,10 +1,11 @@
 #%%
+import time
 from selenium import webdriver
 browser = webdriver.Firefox()
 
 # %%
 url = 'https://en.wikipedia.org/wiki/Nicolas_Cage_filmography'
 browser.get(url)
-sleep(5)
+time.sleep(5)
 browser.close()
 # %%
