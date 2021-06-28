98bf389a5f65e38dd9442839e10a190dc759e89d
José Paulo <petrybr@gmail.com>
2021-06-28 16:17:56 -0300

    working

--- scraper.py
+++ scraper.py
@@ -1,11 +1,11 @@
-#%%
 import time
+import pandas as pd
 from selenium import webdriver
 browser = webdriver.Firefox()
-
-# %%
 url = 'https://en.wikipedia.org/wiki/Nicolas_Cage_filmography'
 browser.get(url)
 time.sleep(5)
-browser.close()
-# %%
+filmography = browser.find_element_by_xpath('/html/body/div[3]/div[3]/div[5]/div[1]/table[1]')
+df = pd.read_html('<table>' + filmography.get_attribute('innerHTML') + '</table>')[0]
+df.to_csv('nicolas_cages_filmography.csv', sep=';', index=False)
+browser.quit()
